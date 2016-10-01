df02e07ddf993341c7627d0ad5356bc75bd28f1a
Brad19 <btbrad1979@gmail.com>
2016-10-01 14:32:22 +1000

    SpeakGoodbye and SpeakHello.js are added

---
+++ mod4_solution/SpeakHello.js
@@ -0,0 +1,8 @@
+(function(window) {
+	var helloSpeaker = {};
+	helloSpeaker.speak = function speak(name) {
+	  console.log(speakWord + " " + name);
+	};
+	window.helloSpeaker = helloSpeaker;
+	var speakWord = "Hello";
+})(window);
